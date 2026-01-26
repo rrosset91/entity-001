55134cc5a25fc9db996f2d34c0491ce4613eb706
Roger Rosset <rrosset91@gmail.com>
2026-01-26 16:28:23 +0000

    Initial commit: ENTITY-001 project scaffolding

---
+++ src/routes/+page.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	// Temporary placeholder
+</script>
+
+<svelte:head>
+	<title>ENTITY-001</title>
+	<meta name="description" content="A mysterious digital entity that seems to know all" />
+</svelte:head>
+
+<div class="app">
+	<h1>ENTITY-001</h1>
+	<p>Project scaffolding in progress...</p>
+</div>
+
+<style>
+	.app {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		min-height: 100vh;
+		padding: 2rem;
+	}
+	
+	h1 {
+		font-size: 4rem;
+		margin-bottom: 1rem;
+	}
+</style>
